92c2bdf2fc8cd20d8c03350dbab4221d34f66014
Farhan Muhammad <farhnmh@gmail.com>
2019-05-08 10:30:47 +0700

    Version 1.0
    There are source codes that every client can connect to server with a new thread. There is a how to play menu for a player that do not know about how to play it.

---
+++ MinusMath - Client.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+public class Client
+{
+    public static void Main()
+    {
+        try
+        {
+            TcpClient client = new TcpClient("10.252.39.170", 8080);
+            Console.WriteLine(" [Game Started...]");
+
+            StreamReader reader = new StreamReader(client.GetStream());
+            StreamWriter writer = new StreamWriter(client.GetStream());
+
+            string option;
+            float stockNumber;
+            float randomAnswer1;
+            float randomAnswer2;
+            float randomAnswer3;
+
+            Console.Write(" [Are You Ready ?] (Y) for accept / (N) for show you 'how to play' : ");
+            string confirm1 = Console.ReadLine();
+            writer.WriteLine(confirm1);
+            writer.Flush();
+
+            if (confirm1 != "Y")
+            {
+                Console.WriteLine("\n [HOW TO PLAY]");
+                string menu1 = reader.ReadLine();
+                Console.WriteLine(menu1);
+                string menu2 = reader.ReadLine();
+                Console.WriteLine(menu2);
+                string menu3 = reader.ReadLine();
+                Console.WriteLine(menu3);
+                string menu4 = reader.ReadLine();
+                Console.WriteLine(menu4);
+
+                Console.Write("\n [Are You Ready ?] (Y) for accept / (N) for exit : ");
+                string confirm2 = Console.ReadLine();
+                writer.WriteLine(confirm2);
+                writer.Flush();
+
+                if (confirm2 != "Y")
+                {
+                    reader.Close();
+                    writer.Close();
+                    client.Close();
+                }
+            }
+
+            string question = reader.ReadLine();
+            stockNumber = Convert.ToInt32(question);
+            Console.WriteLine("\n The Stock Number is " + stockNumber);
+
+            while (true)
+            {
+                string answer1 = reader.ReadLine();
+                randomAnswer1 = Convert.ToInt32(answer1);
+                Console.WriteLine(" A. " + randomAnswer1);
+
+                string answer2 = reader.ReadLine();
+                randomAnswer2 = Convert.ToInt32(answer2);
+                Console.WriteLine(" B. " + randomAnswer2);
+
+                string answer3 = reader.ReadLine();
+                randomAnswer3 = Convert.ToInt32(answer3);
+                Console.WriteLine(" C. " + randomAnswer3);
+
+                Console.Write("\n [Choose The Random Number as Your Answer] A/B/C/enter for reshuffle : ");
+                option = Console.ReadLine();
+                writer.WriteLine(option);
+                writer.Flush();
+
+                if (option != "A" && option != "B" && option != "C")
+                {
+                    answer1 = reader.ReadLine();
+                    randomAnswer1 = Convert.ToInt32(answer1);
+                    Console.WriteLine(" A. " + randomAnswer1);
+
+                    answer2 = reader.ReadLine();
+                    randomAnswer2 = Convert.ToInt32(answer2);
+                    Console.WriteLine(" B. " + randomAnswer2);
+
+                    answer3 = reader.ReadLine();
+                    randomAnswer3 = Convert.ToInt32(answer3);
+                    Console.WriteLine(" C. " + randomAnswer3);
+
+                    Console.Write("\n [Choose The Random Number as Your Answer] A/B/C/enter for reshuffle : ");
+                    option = Console.ReadLine();
+                }
+            }
+
+            //reader.Close();
+            //writer.Close();
+            //client.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
---
+++ MinusMath - Server.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+public class Server
+{
+    private static void CalculatorClient(object argument)
+    {
+        TcpClient client = (TcpClient)argument;
+
+        try
+        {
+            StreamReader reader = new StreamReader(client.GetStream());
+            StreamWriter writer = new StreamWriter(client.GetStream());
+
+            string option;
+            float firstStockNumber;
+            float randomAnswer1;
+            float randomAnswer2;
+            float randomAnswer3;
+            Random randomAnswer = new Random();
+            Random randomStock = new Random();
+
+            string confirm1 = reader.ReadLine();
+            if (confirm1 != "Y")
+            {
+                string menu1 = " 1. Server will send you a question (stock number)";
+                writer.WriteLine(menu1);
+                writer.Flush();
+
+                string menu2 = " 2. After that, you will get three number that you can use one of them as your answer, and send it";
+                writer.WriteLine(menu2);
+                writer.Flush();
+
+                string menu3 = " 3. With your answer, you have to reduce the stock number to zero";
+                writer.WriteLine(menu3);
+                writer.Flush();
+
+                string menu4 = " 4. If you don't get the exact number, you can reshuffle the random number";
+                writer.WriteLine(menu4);
+                writer.Flush();
+
+                string confirm2 = reader.ReadLine();
+                if (confirm2 != "Y")
+                {
+                    Console.WriteLine(" There is a player out");
+                }
+            }
+
+            firstStockNumber = randomStock.Next(90, 100);
+            Console.WriteLine(" Sending a Question (Stock Number) : " + firstStockNumber);
+            writer.WriteLine(firstStockNumber);
+            writer.Flush();
+
+            while (true)
+            {
+                randomAnswer1 = randomAnswer.Next(1, 11);
+                writer.WriteLine(randomAnswer1);
+                writer.Flush();
+
+                randomAnswer2 = randomAnswer.Next(1, 11);
+                while (randomAnswer2 == randomAnswer1)
+                {
+                    randomAnswer2 = randomAnswer.Next(1, 11);
+                    Console.WriteLine(" Random Answer 2 is same with Random Answer 1");
+                }
+                writer.WriteLine(randomAnswer2);
+                writer.Flush();
+
+                randomAnswer3 = randomAnswer.Next(1, 11);
+                while (randomAnswer3 == randomAnswer1 || randomAnswer3 == randomAnswer2)
+                {
+                    randomAnswer3 = randomAnswer.Next(1, 11);
+                    Console.WriteLine(" Random Answer 3 is same with Random Answer 1 and Random Answer 2");
+                }
+                writer.WriteLine(randomAnswer3);
+                writer.Flush();
+
+                option = reader.ReadLine();
+                if (option != "A" && option != "B" && option != "C")
+                {
+                    randomAnswer1 = randomAnswer.Next(1, 11);
+                    writer.WriteLine(randomAnswer1);
+                    writer.Flush();
+
+                    randomAnswer2 = randomAnswer.Next(1, 11);
+                    writer.WriteLine(randomAnswer2);
+                    writer.Flush();
+
+                    randomAnswer3 = randomAnswer.Next(1, 11);
+                    writer.WriteLine(randomAnswer3);
+                    writer.Flush();
+                }
+            }
+        }
+        catch (IOException)
+        {
+            Console.WriteLine(" There is a player out\n");
+        }
+        if (client != null)
+        {
+            client.Close();
+        }
+    }
+
+    public static void Main()
+    {
+        TcpListener listener = null;
+        try
+        {
+            listener = new TcpListener(IPAddress.Parse("10.252.39.170"), 8080);
+            listener.Start();
+
+            Console.WriteLine(" [MINUSMATH]\n");
+
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                Console.WriteLine(" [Ada Player Masuk...]");
+                Thread newThread = new Thread(CalculatorClient);
+
+                newThread.Start(client);
+            }
+        }
+
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+
+        finally
+        {
+            if (listener != null)
+            {
+                listener.Stop();
+            }
+        }
+    }
+}
